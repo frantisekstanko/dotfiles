ca2ba04e55926d73760bb14b397dcde6e470aff4
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-11 16:52:02 +0200

    nvim: telescope: <Esc> closes the popup
    I will see how this mode works for me.
    I may change my mind later.

--- .config/nvim/lua/main/plugins/enabled/telescope.lua
+++ .config/nvim/lua/main/plugins/enabled/telescope.lua
@@ -10,7 +10,16 @@ return {
     },
     config = function()
         local telescope = require("telescope")
+        local actions = require("telescope.actions")
+
         telescope.setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<Esc>"] = actions.close,
+                    },
+                },
+            },
             extensions = {
                 fzf = {
                     fuzzy = true,
