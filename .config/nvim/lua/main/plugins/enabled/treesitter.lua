580d97f41a23e49d8a8ef5982821d1ed9fb4bcf1
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-11 13:20:12 +0200

    nvim: enable treesitter highlighting
    Setting additional_vim_regex_highlighting to true would run `:h syntax`
    and tree-sitter at the same time, which may slow down the editor,
    and you may see some duplicate highlights.

--- .config/nvim/lua/main/plugins/enabled/treesitter.lua
+++ .config/nvim/lua/main/plugins/enabled/treesitter.lua
@@ -5,6 +5,10 @@ return {
         require("nvim-treesitter.configs").setup({
             auto_install = true,
             sync_install = false,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
         })
     end,
 }
