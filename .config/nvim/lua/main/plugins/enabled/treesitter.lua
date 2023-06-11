1f1d1c9a72199d633225ef407ec2975ffa7d75f7
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-11 13:19:50 +0200

    nvim: install treesitter parsers automatically
    And also asynchronously, thanks to sync_install = false.
    This is at the moment the default, but I like to have it
    here explicitly, to remind me of the implications.

--- .config/nvim/lua/main/plugins/enabled/treesitter.lua
+++ .config/nvim/lua/main/plugins/enabled/treesitter.lua
@@ -3,6 +3,8 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            sync_install = false,
         })
     end,
 }
