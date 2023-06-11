94710b5018cde942c91be60928c1f27a7b92f02d
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-11 16:49:39 +0200

    nvim: telescope: use fzf-native
    fzf-native is a c port of fzf

--- .config/nvim/lua/main/plugins/enabled/telescope.lua
+++ .config/nvim/lua/main/plugins/enabled/telescope.lua
@@ -3,8 +3,24 @@ return {
     branch = "0.1.x",
     dependencies = {
         { "nvim-lua/plenary.nvim" },
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
     },
     config = function()
         local telescope = require("telescope")
+        telescope.setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
+            },
+        })
+
+        telescope.load_extension("fzf")
     end,
 }
