6e9e255811c123ba1017bf0822d1726369075889
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-11 13:32:42 +0200

    nvim: enable treesitter's incremental selection

--- .config/nvim/lua/main/plugins/enabled/treesitter.lua
+++ .config/nvim/lua/main/plugins/enabled/treesitter.lua
@@ -20,6 +20,15 @@ return {
                 end,
                 additional_vim_regex_highlighting = false,
             },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
         })
     end,
 }
