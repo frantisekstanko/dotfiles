e98a5aeee7ec10071a780573ecf81bca9fea8faf
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-13 19:19:58 +0200

    nvim: enable transparent background
    Makes it look a little bit better, provided that the terminal has the correct
    color.

--- .config/nvim/lua/main/plugins/enabled/gruvbox.lua
+++ .config/nvim/lua/main/plugins/enabled/gruvbox.lua
@@ -5,6 +5,7 @@ return {
     config = function()
         require("gruvbox").setup({
             bold = false,
+            transparent_mode = true,
         })
 
         vim.cmd.colorscheme("gruvbox")
