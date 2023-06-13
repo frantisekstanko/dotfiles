fded4260fa48571804985214887aea88fa4b3bb5
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-13 14:15:18 +0200

    nvim: lsp: add rust_analyzer

--- .config/nvim/lua/main/plugins/enabled/lspconfig/init.lua
+++ .config/nvim/lua/main/plugins/enabled/lspconfig/init.lua
@@ -3,5 +3,6 @@ return {
     config = function()
         require("main.plugins.enabled.lspconfig.servers.bashls")
         require("main.plugins.enabled.lspconfig.servers.lua_ls")
+        require("main.plugins.enabled.lspconfig.servers.rust_analyzer")
     end,
 }
