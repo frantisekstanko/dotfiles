78a7f153ebedea08871b574ee4a6732efe3ec17d
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-13 14:11:35 +0200

    nvim: lua_ls: treat vim as a global variable
    This is useful for editing nvim lua config files.

--- .config/nvim/lua/main/plugins/enabled/lspconfig/servers/lua_ls.lua
+++ .config/nvim/lua/main/plugins/enabled/lspconfig/servers/lua_ls.lua
@@ -4,5 +4,10 @@ local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 lspconfig["lua_ls"].setup({
     capabilities = lsp_capabilities,
     settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
     },
 })
