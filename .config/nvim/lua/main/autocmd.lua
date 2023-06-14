8db06bdd32f6592dc3261a16f693004e98d6d6a1
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-15 00:22:24 +0200

    nvim: show startup time on startup

--- .config/nvim/lua/main/autocmd.lua
+++ .config/nvim/lua/main/autocmd.lua
@@ -1,5 +1,13 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+        local stats = require("lazy").stats()
+        print("Loaded in " .. math.floor(stats.startuptime + 0.5) .. " ms")
+    end,
+})
+
 autocmd("FileType", {
     pattern = "help",
     callback = function()
