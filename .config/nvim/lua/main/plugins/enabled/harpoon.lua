fce220d3fec09da6cfaf852e77eee7f95eeb67d7
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-06-14 21:50:31 +0200

    nvim: add harpoon

---
+++ .config/nvim/lua/main/plugins/enabled/harpoon.lua
@@ -0,0 +1,50 @@
+return {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        {
+            "<leader>a",
+            function()
+                require("harpoon.mark").add_file()
+            end,
+            desc = "Mark file",
+        },
+        {
+            "<C-e>",
+            function()
+                require("harpoon.ui").toggle_quick_menu()
+            end,
+            desc = "List of marked files",
+        },
+        {
+            "<C-j>",
+            function()
+                require("harpoon.ui").nav_file(1)
+            end,
+            desc = "Go to marked file 1",
+        },
+        {
+            "<C-k>",
+            function()
+                require("harpoon.ui").nav_file(2)
+            end,
+            desc = "Go to marked file 2",
+        },
+        {
+            "<C-l>",
+            function()
+                require("harpoon.ui").nav_file(3)
+            end,
+            desc = "Go to marked file 3",
+        },
+        {
+            "<C-;>",
+            function()
+                require("harpoon.ui").nav_file(4)
+            end,
+            desc = "Go to marked file 4",
+        },
+    },
+    opts = {
+    },
+}
