a667bb7fa518398433782f8f18b9340297a1ec38
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-07-01 22:39:40 +0200

    nvim: telescope: disable fuzzy mode
    Non-fuzzy mode yields better result for the way I use search.

--- .config/nvim/lua/main/plugins/enabled/telescope.lua
+++ .config/nvim/lua/main/plugins/enabled/telescope.lua
@@ -37,7 +37,7 @@ return {
             },
             extensions = {
                 fzf = {
-                    fuzzy = true,
+                    fuzzy = false,
                     override_generic_sorter = true,
                     override_file_sorter = true,
                     case_mode = "smart_case",
