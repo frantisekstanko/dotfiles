519efca44d7fc3131688ebaf153d514be9558225
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-07-01 04:02:11 +0200

    nvim: add snippet engine to cmp
    Snippet engine seems to be required for LSP autocompletion.
    vim-vsnip seems to be enough for this feature to work correctly.

--- .config/nvim/lua/main/plugins/enabled/cmp.lua
+++ .config/nvim/lua/main/plugins/enabled/cmp.lua
@@ -2,11 +2,17 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/vim-vsnip",
     },
     config = function()
         local cmp = require("cmp")
 
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body)
+                end,
+            },
             mapping = {
                 ["<C-u>"] = cmp.mapping.select_prev_item(),
                 ["<C-d>"] = cmp.mapping.select_next_item(),
