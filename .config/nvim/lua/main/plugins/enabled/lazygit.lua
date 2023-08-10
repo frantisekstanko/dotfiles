d3b13f137c2908348c39bb502c2e8055c5c5336e
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-08-10 19:20:43 +0200

    nvim: make lazygit borders the same as telescope's
    I really like consistency.

--- .config/nvim/lua/main/plugins/enabled/lazygit.lua
+++ .config/nvim/lua/main/plugins/enabled/lazygit.lua
@@ -1,5 +1,17 @@
 return {
     "kdheepak/lazygit.nvim",
+    config = function()
+        vim.g.lazygit_floating_window_border_chars = {
+            "┏",
+            "━",
+            "┓",
+            "┃",
+            "┛",
+            "━",
+            "┗",
+            "┃",
+        }
+    end,
     keys = {
         {
             "<leader>cc",
