2b4a9282bd1534a6d611fce938c7c301c5e9f4c1
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-09-28 16:15:03 +0200

    git.nvim now natively supports winbar
    There is no need to toggle barbecue anymore.

--- .config/nvim/lua/main/plugins/enabled/git.lua
+++ .config/nvim/lua/main/plugins/enabled/git.lua
@@ -3,13 +3,13 @@ return {
     config = function()
         require("git").setup({
             default_mappings = false,
+            winbar = true,
         })
     end,
     keys = {
         {
             "<leader>b",
             function()
-                require("barbecue.ui").toggle(false)
                 vim.cmd("GitBlame")
             end,
             desc = "Git blame",
