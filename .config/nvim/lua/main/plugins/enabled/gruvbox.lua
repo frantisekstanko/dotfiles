8adfdb7405ab11bdec92d4d111d0402a215dad31
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-10-01 20:27:34 +0200

    move gruvbox dependency to gruvbox.lua

--- .config/nvim/lua/main/plugins/enabled/gruvbox.lua
+++ .config/nvim/lua/main/plugins/enabled/gruvbox.lua
@@ -9,5 +9,18 @@ return {
         })
 
         vim.cmd.colorscheme("gruvbox")
+
+        local colors =
+            require("gruvbox.palette").get_base_colors({}, "dark", "")
+
+        local dark = colors.bg0
+        local green = colors.green
+        local orange = colors.orange
+        local light = colors.fg0
+
+        vim.api.nvim_set_hl(0, "BufferCurrent", { fg = dark, bg = green })
+        vim.api.nvim_set_hl(0, "BufferCurrentMod", { fg = dark, bg = orange })
+        vim.api.nvim_set_hl(0, "BufferInactive", { fg = light })
+        vim.api.nvim_set_hl(0, "BufferInactiveMod", { fg = orange })
     end,
 }
