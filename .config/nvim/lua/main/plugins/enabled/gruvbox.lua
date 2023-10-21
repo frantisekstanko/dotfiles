67bd3a1622c0b098f8670500530d0535caab0048
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-10-21 23:05:49 +0200

    refactor: set hl groups in gruvbox's overrides

--- .config/nvim/lua/main/plugins/enabled/gruvbox.lua
+++ .config/nvim/lua/main/plugins/enabled/gruvbox.lua
@@ -3,13 +3,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("gruvbox").setup({
-            bold = false,
-            transparent_mode = true,
-        })
-
-        vim.cmd.colorscheme("gruvbox")
-
         local colors = require("gruvbox").palette
 
         local dark = colors.dark0
@@ -17,9 +10,17 @@ return {
         local orange = colors.bright_orange
         local light = colors.light0
 
-        vim.api.nvim_set_hl(0, "BufferCurrent", { fg = dark, bg = green })
-        vim.api.nvim_set_hl(0, "BufferCurrentMod", { fg = dark, bg = orange })
-        vim.api.nvim_set_hl(0, "BufferInactive", { fg = light })
-        vim.api.nvim_set_hl(0, "BufferInactiveMod", { fg = orange })
+        require("gruvbox").setup({
+            bold = false,
+            transparent_mode = true,
+            overrides = {
+                BufferCurrent = { fg = dark, bg = green },
+                BufferCurrentMod = { fg = dark, bg = orange },
+                BufferInactive = { fg = light },
+                BufferInactiveMod = { fg = orange },
+            },
+        })
+
+        vim.cmd.colorscheme("gruvbox")
     end,
 }
