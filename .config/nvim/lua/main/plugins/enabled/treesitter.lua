ecd3e6e2d632db963d629bfcff2098db24e11429
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-10-21 23:02:53 +0200

    rename unused variable to underscore

--- .config/nvim/lua/main/plugins/enabled/treesitter.lua
+++ .config/nvim/lua/main/plugins/enabled/treesitter.lua
@@ -7,7 +7,7 @@ return {
             sync_install = false,
             highlight = {
                 enable = true,
-                disable = function(lang, buf)
+                disable = function(_, buf)
                     local max_filesize = 1024 * 100 -- 100 KB
                     local ok, stats =
                         pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
