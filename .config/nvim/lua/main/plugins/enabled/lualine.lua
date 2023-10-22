408ba4217dffa409111c9171ca13b05be3e54085
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-10-22 23:09:21 +0200

    refactor lualine config to use opts

--- .config/nvim/lua/main/plugins/enabled/lualine.lua
+++ .config/nvim/lua/main/plugins/enabled/lualine.lua
@@ -3,37 +3,35 @@ return {
     dependencies = {
         "arkav/lualine-lsp-progress",
     },
-    config = function()
-        require("lualine").setup({
-            options = {
-                icons_enabled = false,
-                theme = "gruvbox",
-                section_separators = {
-                    left = "",
-                    right = "",
-                },
-                component_separators = {
-                    left = "┃",
-                    right = "┃",
-                },
+    opts = {
+        options = {
+            icons_enabled = false,
+            theme = "gruvbox",
+            section_separators = {
+                left = "",
+                right = "",
             },
-            sections = {
-                lualine_b = { "branch", "diff" },
-                lualine_c = {
-                    { "filename" },
-                    { "lsp_progress" },
-                    {
-                        "diagnostics",
-                        sources = { "nvim_lsp" },
-                        symbols = {
-                            error = "ERR:",
-                            warn = "WARN:",
-                            info = "INFO:",
-                            hint = "HINT:",
-                        },
+            component_separators = {
+                left = "┃",
+                right = "┃",
+            },
+        },
+        sections = {
+            lualine_b = { "branch", "diff" },
+            lualine_c = {
+                { "filename" },
+                { "lsp_progress" },
+                {
+                    "diagnostics",
+                    sources = { "nvim_lsp" },
+                    symbols = {
+                        error = "ERR:",
+                        warn = "WARN:",
+                        info = "INFO:",
+                        hint = "HINT:",
                     },
                 },
             },
-        })
-    end,
+        },
+    },
 }
