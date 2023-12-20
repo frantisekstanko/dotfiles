1bbf15b0499deb42d1dfd3670490d31804db8fa6
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-12-20 23:41:42 +0100

    use config to initialize lualine
    I want to change some of the colors of lualine, which I will read from
    the gruvbox theme.

--- .config/nvim/lua/main/plugins/enabled/lualine.lua
+++ .config/nvim/lua/main/plugins/enabled/lualine.lua
@@ -1,33 +1,35 @@
 return {
     "nvim-lualine/lualine.nvim",
-    opts = {
-        options = {
-            icons_enabled = false,
-            theme = "gruvbox",
-            section_separators = {
-                left = "",
-                right = "",
+    config = function()
+        require("lualine").setup({
+            options = {
+                icons_enabled = false,
+                theme = "gruvbox",
+                section_separators = {
+                    left = "",
+                    right = "",
+                },
+                component_separators = {
+                    left = "┃",
+                    right = "┃",
+                },
             },
-            component_separators = {
-                left = "┃",
-                right = "┃",
-            },
-        },
-        sections = {
-            lualine_b = { "branch", "diff" },
-            lualine_c = {
-                { "filename" },
-                {
-                    "diagnostics",
-                    sources = { "nvim_lsp" },
-                    symbols = {
-                        error = "ERR:",
-                        warn = "WARN:",
-                        info = "INFO:",
-                        hint = "HINT:",
+            sections = {
+                lualine_b = { "branch", "diff" },
+                lualine_c = {
+                    { "filename" },
+                    {
+                        "diagnostics",
+                        sources = { "nvim_lsp" },
+                        symbols = {
+                            error = "ERR:",
+                            warn = "WARN:",
+                            info = "INFO:",
+                            hint = "HINT:",
+                        },
                     },
                 },
             },
-        },
-    },
+        })
+    end,
 }
