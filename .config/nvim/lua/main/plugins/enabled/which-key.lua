a52844771702e5ef4b5bc503df57e0ad855fba27
Frantisek Stanko <frantisek.stanko@protonmail.com>
2024-10-03 21:58:11 +0200

    migrate to which-key v3
    This also merges all keymaps into the one plugin config file, as this is the
    easiest way to go with v3 and I don't mind the coupling.

--- .config/nvim/lua/main/plugins/enabled/which-key.lua
+++ .config/nvim/lua/main/plugins/enabled/which-key.lua
@@ -1,22 +1,147 @@
 return {
     "folke/which-key.nvim",
     lazy = false,
-    opts = {},
+    opts = {
+        preset = "classic",
+        delay = 1000,
+    },
     keys = {
         {
-            desc = "Show keymaps",
             "<leader>k",
             function()
-                vim.cmd("WhichKey")
+                require("which-key").show({
+                    global = false,
+                })
             end,
+            desc = "Show all keymaps",
+        },
+        {
+            "<leader>p",
+            function()
+                vim.cmd("let @+ = fnamemodify(expand('%'), ':~:.')")
+            end,
+            desc = "Copy file path to clipboard",
+        },
+        {
+            "<leader>U",
+            function()
+                require("lazy").sync()
+            end,
+            desc = "Sync plugins",
+        },
+        {
+            "<leader>f",
+            function()
+                vim.lsp.buf.format()
+            end,
+            desc = "Format file using LSP",
+        },
+        {
+            "<leader>r",
+            function()
+                vim.lsp.buf.rename()
+            end,
+            desc = "Rename all references using LSP",
+        },
+        {
+            "<leader>ca",
+            function()
+                vim.lsp.buf.code_action()
+            end,
+            desc = "Code actions of LSP",
+        },
+        {
+            "<leader>lr",
+            function()
+                vim.lsp.stop_client(vim.lsp.get_active_clients())
+                vim.cmd("edit")
+                require("notify")(" Lsp restarted", vim.log.levels.OK)
+            end,
+            desc = "Restart LSP",
+        },
+        {
+            "[d",
+            function()
+                vim.diagnostic.goto_prev()
+            end,
+            desc = "Go to previous diagnostic",
+        },
+        {
+            "]d",
+            function()
+                vim.diagnostic.goto_next()
+            end,
+            desc = "Go to next diagnostic",
+        },
+        {
+            "K",
+            function()
+                vim.lsp.buf.hover()
+            end,
+            desc = "Show hover info from LSP",
+        },
+        {
+            "<leader>w",
+            function()
+                vim.cmd('normal! "+yiw')
+            end,
+            desc = "Copy word under cursor into system clipboard",
+        },
+        {
+            "<leader>R",
+            function()
+                local word_under_cursor = vim.fn.expand("<cword>")
+                local go_left_three_times = vim.api.nvim_replace_termcodes(
+                    "<Left><Left><Left>",
+                    true,
+                    false,
+                    true
+                )
+                vim.api.nvim_feedkeys(
+                    ":%s/" .. word_under_cursor .. "//gI" .. go_left_three_times,
+                    "n",
+                    false
+                )
+            end,
+            desc = "Substitute word under cursor",
+        },
+        {
+            "<leader>M",
+            function()
+                local filelist = vim.fn.systemlist(
+                    "git diff --name-only --relative main...HEAD"
+                )
+
+                for _, file in ipairs(filelist) do
+                    if vim.fn.filereadable(file) == 1 then
+                        vim.cmd("edit " .. file)
+                    end
+                end
+            end,
+            desc = "Open all modified files on current branch",
+        },
+        {
+            "<leader>u",
+            function()
+                local uuid = vim.fn.systemlist("uuidgen")[1]
+                vim.api.nvim_put({ uuid }, "c", false, true)
+            end,
+            desc = "Insert UUID into current position",
         },
     },
     config = function()
         local wk = require("which-key")
 
-        wk.register(require("main.keymaps.normal_mode"))
-        wk.register(require("main.keymaps.normal_and_visual_mode"), {
+        wk.add({
             mode = { "n", "v" },
+            { "<leader>P", '"+p', desc = "Paste from system clipboard" },
+            { "<leader>Y", '"+Y', desc = "YANK to system clipboard" },
+            {
+                "<leader>d",
+                '"_d',
+                desc = "Delete without replacing the unnamed register",
+            },
+            { "<leader>y", '"+y', desc = "yank to system clipboard" },
         })
     end,
 }
