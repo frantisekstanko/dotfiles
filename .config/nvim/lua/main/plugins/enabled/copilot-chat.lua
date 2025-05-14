c550534d28d238e9a715d0d44320352efc690e8a
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-05-14 14:10:50 +0200

    update copilot-chat config for the latest version

--- .config/nvim/lua/main/plugins/enabled/copilot-chat.lua
+++ .config/nvim/lua/main/plugins/enabled/copilot-chat.lua
@@ -1,26 +1,9 @@
 return {
     "CopilotC-Nvim/CopilotChat.nvim",
-    opts = {},
-    build = function()
-        vim.cmd("UpdateRemotePlugins")
-    end,
-    event = "VeryLazy",
-    keys = {
-        {
-            desc = "Copilot Chat for current buffer",
-            "<leader>cc",
-            function()
-                vim.ui.input({
-                    prompt = "Copilot Chat for current buffer: ",
-                }, function(input)
-                    if input == nil or input == "" then
-                        print(" ")
-                        return
-                    end
-
-                    vim.cmd("CopilotChatBuffer " .. input)
-                end)
-            end,
-        },
+    dependencies = {
+        { "github/copilot.vim" },
+        { "nvim-lua/plenary.nvim", branch = "master" },
     },
+    build = "make tiktoken",
+    opts = {},
 }
