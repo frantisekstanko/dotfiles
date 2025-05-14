c7f915d289effe4f31080d20082372b433d5306d
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-05-14 14:11:06 +0200

    disable mixed type check in intelephense

--- .config/nvim/lua/main/plugins/enabled/lspconfig.lua
+++ .config/nvim/lua/main/plugins/enabled/lspconfig.lua
@@ -57,7 +57,7 @@ return {
                     },
                     diagnostics = {
                         relaxedTypeCheck = false,
-                        noMixedTypeCheck = false,
+                        noMixedTypeCheck = true,
                     },
                     compatibility = {
                         preferPsalmPhpstanPrefixedAnnotations = true,
