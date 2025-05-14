01416fc1d509ca24eab4648d0a67e349147d670e
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-05-14 14:31:34 +0200

    accept copilot's suggestions with C-k instead of TAB

--- .config/nvim/lua/main/plugins/enabled/copilot-chat.lua
+++ .config/nvim/lua/main/plugins/enabled/copilot-chat.lua
@@ -1,5 +1,12 @@
 return {
     "CopilotC-Nvim/CopilotChat.nvim",
+    config = function()
+        vim.keymap.set("i", "<C-k>", 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false,
+        })
+        vim.g.copilot_no_tab_map = true
+    end,
     dependencies = {
         { "github/copilot.vim" },
         { "nvim-lua/plenary.nvim", branch = "master" },
