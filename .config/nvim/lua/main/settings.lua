53bad52881e6847e00f25ced30fea224068a11dd
Frantisek Stanko <frantisek.stanko@protonmail.com>
2025-10-03 01:23:27 +0200

    disable netrw

--- .config/nvim/lua/main/settings.lua
+++ .config/nvim/lua/main/settings.lua
@@ -25,6 +25,8 @@ vim.opt.backupcopy = "yes"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 vim.diagnostic.config({
     virtual_text = {
